9ebb9c6831266da4a14af4f9db99bb59ffe5ef95
Rennerp <rekefa@gmail.com>
2021-02-27 19:13:37 -0600

    Se agregan mejoras a las descripcciones

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,28 @@ const Index = () => (
         <div className="card bg-secondary card-body text-white">
           <div className="row">
             <div className="col-md-4">
-              <img src="profile.png" className="img-fluid" />
+              <img
+                src="profile.png"
+                className="img-fluid"
+                style={{ height: "50vh" }}
+              />
             </div>
             <div className="col-md-8">
-              <h1>Renner Poveda</h1>
-              <h3>FullStack Developer</h3>
-              <p>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="/hireme">Hire Me</a>
+              <h1>Renner Jesus Garcia Poveda</h1>
+              <h5>Managua, Nicaragua</h5>
+              <h5>Telf: +505 7518 - 7141</h5>
+              <h5>Email: rekefa@gmail.com</h5>
+              <div className="py-4">
+                <h3>FullStack Developer</h3>
+                <p>
+                  Software developer, through all my years of experience, I have
+                  discovered many qualities about myself, besides my passion for
+                  software development and new technologies, I consider myself
+                  to be a person with a lot of values empathy, responsable,
+                  honest, team player, with no fear of new challenges,
+                  detail-oriented and very proactive.
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -59,6 +71,8 @@ const Index = () => (
         </div>
       </div>
     </div>
+
+    {/** Projects section **/}
   </Layout>
 );
 
